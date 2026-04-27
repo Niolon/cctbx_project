176824dd7c5aadb6d315bcdf8b6ed940971f4842
Paul Niklas Ruth <paul.n.ruth@durham.ac.uk>
2026-04-27 16:18:41 +0100

    remove repeated registrations

--- smtbx/refinement/constraints/boost_python/nomore.cpp
+++ smtbx/refinement/constraints/boost_python/nomore.cpp
@@ -41,12 +41,8 @@ namespace smtbx { namespace refinement { namespace constraints {
       }
     };
     void wrap_nomore() {
-      {
-        using namespace scitbx::boost_python::container_conversions;
-        tuple_mapping_variable_capacity<af::shared<double> >();
-        tuple_mapping_variable_capacity<af::shared<int> >();
-        tuple_mapping_variable_capacity<af::shared<scalar_parameter *> >();
-      }
+      
+      using namespace scitbx::boost_python::container_conversions;   
       nomore_wrapper::wrap();
     }
   }
